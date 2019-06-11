a402be3dc204a7cbd25ca3630c1851c0539d85a6
Paweł Recław <pawel.reclaw@gmail.com>
2019-06-11 18:14:11 +0200

    Zapis do pliku

--- src/main/java/com/javagda25/Main.java
+++ src/main/java/com/javagda25/Main.java
@@ -1,7 +1,19 @@
 package com.javagda25;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
 
+        // data.txt
+        // Zapisz linię "Hello world!" (w trybie append)
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("data.txt", true))) {
+            writer.println("Hello World!");
+        } catch (IOException e) {
+            System.err.println("Error, unable to open this file and write to it.");
+        }
     }
 }
